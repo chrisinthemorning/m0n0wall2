f7325550f5147a7443fddf0584c8c1bce23e1fdb
andywhite <andywhite@19744dee-bc3b-d56f-1ba3-c9dc609a10c9>
2011-02-28 23:17:13 +0000

    make scripts executable

--- trunk/go.sh
+++ trunk/go.sh
@@ -10,6 +10,7 @@ wget http://m0n0wall2.googlecode.com/svn/trunk/2makebinaries.sh
 wget http://m0n0wall2.googlecode.com/svn/trunk/3patchtools.sh
 wget http://m0n0wall2.googlecode.com/svn/trunk/4buildkernel.sh
 wget http://m0n0wall2.googlecode.com/svn/trunk/5makeimage.sh
+chmod a+rx *.sh
 
 echo "I will leave you in a bash shell now"
 echo "execute doall.sh or run 1makebuildenv.sh , then 2makebinaries.sh, then 3patchtools.sh etc"
